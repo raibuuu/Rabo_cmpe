a9036c6d32d50d99ffd7841081cd9701710ce9b7
raibuuu <raboraiveneb@gmail.com>
2025-10-11 13:44:43 +0800

    My Python Notes

---
+++ my_package/DataType.py
@@ -0,0 +1,22 @@
+#java
+#45 integer
+#45.1 float
+#45213421312321312 long
+#45.21312321312323123123 double
+#"hello" string
+#'h' char
+myNum1 = 45
+myNum2 = 45.5
+myNum3 = 45123213213123123216545454676576567456456456435645475456535435654765465354354654635435432543634564654653543463465465
+myNum4 = 45.123123123123232
+
+myString = "hello world !@#$%^&*() 12312312327"
+age1 = "21"
+age2 = "25"
+sum = age1 + age2
+print(sum)
+
+myString1 = "Hello"
+myString2 = " World"
+sum = myString1 + myString2
+print(sum)
---
+++ my_package/myFirstPythonFile.py
@@ -0,0 +1,9 @@
+int("Hello Wprld")
+int("Hello Eljaye")
+int ('Hello Eljaye Single Quote')
+int ('Raivene said "Hello, miss na kita!!"')
+int ("Raivene said 'Hello, miss na kita!!'")
+int ("Raivene said Hello, miss na kita!!")
+
+int(input("Enter your name :"))
+
